843ffc1010776ca0812e5dc1092b59c08137ce3a
Aman <122904081+theaman05@users.noreply.github.com>
2023-06-22 11:41:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ def handle_clearchat():
     db.session.commit()
     emit('clearAllChat', broadcast=True)
 
-if __name__=='__main__':
+# if __name__=='__main__':
     # with app.app_context():
     #     db.create_all()
-    socketio.run(app)
+    # socketio.run(app)
